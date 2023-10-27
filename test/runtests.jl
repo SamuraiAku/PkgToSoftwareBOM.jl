65021579829b9b729b81afefac9d6249c81732a9
Simon Avery <savery@ieee.org>
2023-10-27 12:59:14 -0700

    Add a Project and Manifest for the Dummy test packages test. This way should allow this version to keep running the tests succesfully, even if the packages are updated for other tests in later versions of this package.

--- test/runtests.jl
+++ test/runtests.jl
@@ -83,7 +83,7 @@ using UUIDs
             return true
         end
 
-        Pkg.add(url= "https://github.com/SamuraiAku/Dummy4.git")
+        Pkg.activate("./test_environment")
         sbom= generateSPDX(spdxCreationData(rootpackages= filter(p-> (p.first in ["Dummy4"]), Pkg.project().dependencies)), ["DummyRegistry", "General"]);
         # Dummy4 and all its dependencies were created by the author for testing purposes. They have no functional code, just the dependencies
         # Therefore we know exactly what the SBOM should look like and can test for this.
